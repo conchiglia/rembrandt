e7668a24a1e8e6d16a839674963bb8f1de6b3ab4
mantczak04 <mantczakdev@gmail.com>
2026-05-09 21:16:52 +0200

    scene - functional way

--- src/rembrandt/scene.py
+++ src/rembrandt/scene.py
@@ -1,6 +1,7 @@
 import bpy
 from pathlib import Path
 from rembrandt.errors import ModelFileNotFoundError
+from mathutils import Vector, Matrix # type: ignore
 
 def load_obj(obj_path: str) -> None:
     """
@@ -21,3 +22,37 @@ def clear_scene() -> None:
     """
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.delete()
+
+def setup_camera(location: tuple[float, float, float] = (5.0, 5.0, 5.0),
+                 look_at: tuple[float, float, float] = (0.0, 0.0, 0.0), 
+                 focal_length: float = 50.0
+                 ) -> None:
+    """
+    Creates and positions camera pointing towards the 3D model in the scene.
+
+    Args:
+        location: Camera position (x, y, z)
+        look_at: 3D Model position at which camera looks at (x, y, z)
+        focal_length: Camera focal length in mm (default 50mm is standard)
+    """
+
+    #Create a camera
+    camera_data = bpy.data.cameras.new("Camera")
+    camera_obj = bpy.data.objects.new("Camera", camera_data)
+
+    #Add to the scene
+    bpy.context.collection.objects.link(camera_obj)
+
+    #Set up camera location
+    camera_obj.location = location
+
+    #Look at specific point (default: (0,0,0))
+    direction = Vector(look_at) - Vector(location)
+    camera_rotation = direction.to_track_quat('-Z', 'Y') #camera's rotation quaternion
+    camera_obj.rotation_euler = camera_rotation.to_euler()
+
+    #Set up focal length of camera
+    camera_data.lens = focal_length
+
+    #Set up camera as active
+    bpy.context.scene.camera = camera_obj
